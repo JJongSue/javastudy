d5933d474eb128dcf967fcf972e2532391bae430
jspark940321 <jspark940321@gmail.com>
2020-12-28 22:40:36 +0900

    pacakge / import

---
+++ src/main/java/week7/Example.java
@@ -0,0 +1,12 @@
+package week7;
+//import week7.p1.*;
+import week7.p1.hi;
+
+public class Example {
+    public static void main(String[] args) {
+        hi h = new hi();
+        h.hello();
+        Like li = new Like();
+        li.IL();
+    }
+}
