1fe0fe631a7a6122e5860ce1a6cb559f97bd758e
jspark940321 <jspark940321@gmail.com>
2020-12-30 23:33:44 +0900

    add import/static import 예제 및 설명 추가

--- src/main/java/week7/Example.java
+++ src/main/java/week7/Example.java
@@ -2,11 +2,23 @@ package week7;
 //import week7.p1.*;
 import week7.p1.hi;
 
+import static java.lang.Math.abs;
+
+//import week7.p1.StaticClass;
+//import static week7.p1.StaticClass.*;
+import static week7.p1.StaticClass.print;
 public class Example {
+
     public static void main(String[] args) {
         hi h = new hi();
         h.hello();
         Like li = new Like();
         li.IL();
+//        int a = Math.abs(-1);
+        int a = abs(-1);
+
+//        StaticClass.print();
+        print();
     }
+
 }
---
+++ src/main/java/week7/p1/StaticClass.java
@@ -0,0 +1,10 @@
+package week7.p1;
+
+public class StaticClass {
+    public static int MAXVAL = 100;
+    public static int MINVAL = 100;
+    public static void print(){
+        System.out.println("print!!!!");
+    }
+
+}
