d69c95d3bd4dfaf16942873f0de3b6e2faf1bc35
jspark940321 <jspark940321@gmail.com>
2021-01-12 22:55:38 +0900

    try-catch 예제 및 설명

--- src/main/java/week9/ExceptionExample.java
+++ src/main/java/week9/ExceptionExample.java
@@ -4,11 +4,18 @@ public class ExceptionExample {
     public static void main(String[] args) {
         
         try{
-//            ArrayException ae = new ArrayException();
-//            System.out.println(ae.array[0]);
+            ArrayException ae = new ArrayException();
+            System.out.println(ae.array[0]);
 
+        }catch (NullPointerException | InterruptedException){
+            System.out.println("|");
+
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBoundsException");
         } catch (Exception e) {
             System.out.println("error 발생");
+        } finally {
+            System.out.println("finally");
         }
     }
 }
